74d86db5bcd12e4562ce6a153e54cd4cc87f6aea
dc <s10843047@ems.tcust.edu.tw>
2022-07-19 11:33:59 +0800

    add orm

---
+++ application/controllers/transaction.py
@@ -0,0 +1,198 @@
+from datetime import datetime
+from urllib import response
+from flask_restful import Resource, reqparse
+from application.models.member import Customer, Employee, Supplier
+from application.models.transaction import Transaction, ReStock, ReStockDetail
+from application.models.sku import Sku
+from application.models.product import Product
+
+from db import db
+# from db_pyodbc import cnxn
+import uuid
+# import json 
+
+class TransactionAPI(Resource):
+    LIST_URL = '/transaction/<trans_id>'
+    CREATE_URL = '/transaction'
+
+    def get(self, trans_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('employee_id', type=str, required=True, location=['form'])
+        parser.add_argument('customer_id', type=str, required=True, location=['form'])
+        
+        transaction = db.session.query(Transaction).filter(
+            Transaction.trans_id == (trans_id if(trans_id!='all') else Transaction.trans_id)
+        ).all()
+
+        if not transaction:
+            return {'message':'查無此交易紀錄'}
+
+        transaction_json = {
+            'data': [record.to_json() for record in transaction],
+            'title': 'transaction'
+        } 
+        return transaction_json
+    
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('employee_id', type=str, required=True, location=['form'])
+        parser.add_argument('customer_id', type=str, required=True, location=['form'])
+        parser.add_argument('trans_date', type=str, required=False, location=['form'])
+        req_data = parser.parse_args()
+        print(req_data)
+        transaction = Transaction.create(
+            trans_id = str(uuid.uuid4()),
+            employee_id = req_data['employee_id'],
+            customer_id = req_data['customer_id'],
+            trans_date = req_data['trans_date'],
+            total_price = 200000
+        )
+        db.session.add(transaction)
+        db.session.commit()
+        return {'trans_id': transaction.trans_id},
+
+    def put(self, trans_id): 
+        parser = reqparse.RequestParser()
+        parser.add_argument('total_price', type=int, required=True)
+        req_data = parser.parse_args()
+
+        transaction = db.session.query(Transaction).filter(
+            Transaction.trans_id == trans_id
+        ).first()
+        if not transaction:
+            return {'message': f'不存在的trans_id:{trans_id}'}
+        
+        transaction.total_price = req_data['total_price']
+        db.session.add(transaction)
+        db.session.commit()
+        return 
+
+    def delete(self, trans_id):
+        transaction = db.session.query(Transaction).filter(
+            Transaction.trans_id == trans_id
+        ).first()
+        
+        if not transaction:
+            return {'message': f'不存在的trans_id:{trans_id}'}
+
+        db.session.delete(transaction)
+        db.session.commit()
+        return 
+
+class RestockDataAPI(Resource):
+    LIST_URL = '/restock_data/<supplier_id>'
+    CREATE_URL = '/restock_data'
+
+    def get(self, supplier_id):
+        sku = db.session.query(
+                                Sku.sku_id,
+                                Product.product_name,
+                                Sku.sku_code,
+                                Sku.cost
+                            ).join(
+                                Product,
+                                Sku.product_id == Product.product_id,
+                                isouter = True
+                            ).filter(
+                                Product.supplier_id == supplier_id
+                            ).all()
+        json = {
+            'sku': [{
+                        'sku_id': record[0],
+                        'product_name': record[1],
+                        'sku_code': record[2],
+                        'cost': record[3]
+                    } for record in sku]
+        } 
+        return  json
+
+class RestockAPI(Resource):
+    LIST_URL = '/restock/<restock_id>'
+    CREATE_URL = '/restock'
+
+    def get(self, restock_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('employee_id', type=str, required=True, location=['form'])
+        parser.add_argument('customer_id', type=str, required=True, location=['form'])
+        
+        restock = db.session.query(
+                                ReStock, 
+                                Employee.employee_name,
+                                Supplier.supplier_name
+                            ).join(
+                                Employee, 
+                                ReStock.employee_id == Employee.employee_id, 
+                                isouter = True
+                            ).join(
+                                Supplier,
+                                ReStock.supplier_id == Supplier.supplier_id, 
+                                isouter = True
+                            ).filter(
+                                ReStock.restock_id == (restock_id if(restock_id!='all') else ReStock.restock_id)
+                            ).all()
+
+        print(restock)
+        if not restock:
+            return {'message':'查無此交易紀錄'}
+
+        restock_json = {
+            'data': [{
+                            'restock_id': record[0].restock_id,
+                            'employee_id': record[0].employee_id,
+                            'employee_name': record[1],
+                            'supplier_id': record[0].supplier_id,
+                            'supplier_name': record[2],
+                            'restock_date': record[0].restock_date.strftime("%m/%d/%Y, %H:%M:%S"),
+                            'total_price': record[0].total_price
+                        } for record in restock],
+            'title': 'restock'
+        } 
+        return restock_json
+        return 200
+    
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('employee_id', type=str, required=True, location=['form'])
+        parser.add_argument('supplier_id', type=str, required=True, location=['form'])
+        parser.add_argument('restock_date', type=str
+        , required=True, location=['form'])
+        req_data = parser.parse_args()
+        print(type(req_data['restock_date']))
+        restock = ReStock.create(
+            restock_id = str(uuid.uuid4()),
+            employee_id = req_data['employee_id'],
+            supplier_id = req_data['supplier_id'],
+            restock_date = req_data['restock_date'],
+            total_price = 200000
+        )
+        db.session.add(restock)
+        db.session.commit()
+        return {'restock_id': restock.restock_id},
+
+    def put(self, trans_id): 
+        parser = reqparse.RequestParser()
+        parser.add_argument('total_price', type=int, required=True)
+        req_data = parser.parse_args()
+
+        Restock = db.session.query(Restock).filter(
+            Restock.trans_id == trans_id
+        ).first()
+        if not Restock:
+            return {'message': f'不存在的trans_id:{trans_id}'}
+        
+        Restock.total_price = req_data['total_price']
+        db.session.add(Restock)
+        db.session.commit()
+        return 
+
+    def delete(self, trans_id):
+        Restock = db.session.query(Restock).filter(
+            Restock.trans_id == trans_id
+        ).first()
+        
+        if not Restock:
+            return {'message': f'不存在的trans_id:{trans_id}'}
+
+        db.session.delete(Restock)
+        db.session.commit()
+        return 
---
+++ application/models/transaction.py
@@ -0,0 +1,97 @@
+from db import db
+
+
+class Transaction(db.Model):
+    __tablename__ = 'trans_record'
+    __table_args__ = {'mysql_charset': 'utf8mb4'}
+
+    trans_id = db.Column(db.String(36), primary_key=True)
+    employee_id = db.Column(db.String(7), nullable=False)
+    customer_id = db.Column(db.String(7), nullable=False)
+    trans_date = db.Column(db.DateTime(), nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+
+    @classmethod 
+    def create(cls, trans_id, employee_id, customer_id, trans_date, total_price):
+        transaction = Transaction()
+        transaction.trans_id = trans_id
+        transaction.employee_id = employee_id
+        transaction.customer_id = customer_id
+        transaction.trans_date = trans_date
+        transaction.total_price = total_price
+        return transaction
+
+    def to_json(self):
+        json = {
+            'trans_id': self.trans_id,
+            'employee_id': self.employee_id,
+            'customer_id': self.customer_id,
+            'trans_date': self.trans_date.strftime("%m/%d/%Y, %H:%M:%S"),
+            'total_price': self.total_price
+        }
+        return json
+
+
+
+class ReStock(db.Model):
+    __tablename__ = 'restock_record'
+    __table_args__ = {'mysql_charset': 'utf8mb4'}
+
+    restock_id = db.Column(db.String(36), primary_key=True)
+    employee_id = db.Column(db.String(7), nullable=False)
+    supplier_id = db.Column(db.String(7), nullable=False)
+    restock_date = db.Column(db.DateTime(), nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+
+    @classmethod 
+    def create(cls, restock_id, employee_id, supplier_id, restock_date, total_price):
+        restock = ReStock()
+        restock.restock_id = restock_id
+        restock.employee_id = employee_id
+        restock.supplier_id = supplier_id
+        restock.restock_date = restock_date
+        restock.total_price = total_price
+        return restock
+
+    def to_json(self):
+        json = {
+            'restock_id': self.restock_id,
+            'employee_id': self.employee_id,
+            'supplier_id': self.supplier_id,
+            'restock_date': self.restock_date.strftime("%m/%d/%Y, %H:%M:%S"),
+            'total_price': self.total_price
+        }
+        return json
+
+class ReStockDetail(db.Model):
+    __tablename__ = 'restock_detail'
+    __table_args__ = {'mysql_charset': 'utf8mb4'}
+
+    restock_id = db.Column(db.String(36), primary_key=True)
+    sku_id = db.Column(db.String(11), primary_key=True, nullable=False)
+    serial_id = db.Column(db.String(128), nullable=True)
+    sale_price = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    memo = db.Column(db.String(300), nullable=True)
+
+    @classmethod 
+    def create(cls, restock_id, sku_id, serial_id, sale_price, quantity, memo):
+        restock_detail = ReStockDetail()
+        restock_detail.restock_id = restock_id
+        restock_detail.sku_id = sku_id
+        restock_detail.serial_id = serial_id
+        restock_detail.sale_price = sale_price
+        restock_detail.quantity = quantity
+        restock_detail.memo = memo
+        return restock_detail
+
+    def to_json(self):
+        json = {
+            'restock_id': self.restock_id,
+            'sku_id': self.sku_id,
+            'serial_id': self.serial_id,
+            'sale_price': self.sale_price,
+            'quantity': self.quantity,
+            'memo': self.memo
+        }
+        return json
